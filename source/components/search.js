40938acaa4a49fde179f5248fcdc336019b40337
Nick Cox <nick@nickcox.me>
2016-06-02 15:00:17 -0700

    search icon and search box animation

--- source/components/search.js
+++ source/components/search.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-  search() {
+  constructor(props, context) {
+    super(props, context);
+    this.setFocus = this.setFocus.bind(this);
+    this.setBlur = this.setBlur.bind(this);
+    this.state = {
+      focused: false
+    };
   }
+
+  setFocus() {
+    this.setState({ focused: true });
+    this.refs.searchInput.focus();
+  }
+
+  setBlur() {
+    this.setState({ focused: false });
+  }
+
   render() {
+    const state = this.state;
+    const focusedName = this.state.focused ? 'focused' : 'blurred';
+    const inputClassName = `${focusedName} search__input`;
+    const placeholder = state.focused ? 'Search populations by region, country, input zone' : '';
     return (
-      <input
-        type="text"
-        className="search__input"
-        placeholder="Search populations by region, country, or input zone"
-      />
+      <div>
+        <input
+          type="text"
+          ref="searchInput"
+          className={inputClassName}
+          placeholder={placeholder}
+          onFocus={this.setFocus}
+          onBlur={this.setBlur}
+        />
+        <span onClick={this.setFocus} className="icon__magnifying-glass"></span>
+      </div>
     );
   }
 }
