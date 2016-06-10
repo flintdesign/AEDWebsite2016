697bc567685f5c1226a0c6f4cb8851d515779534
Nick Cox <nick@nickcox.me>
2016-06-10 14:29:29 -0700

    draw and style regions WIP

--- source/components/map_container.js
+++ source/components/map_container.js
@@ -8,20 +8,36 @@ export default class MapContainer extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.getCenter = this.getCenter.bind(this);
     this.onZoomEnd = this.onZoomEnd.bind(this);
+    this.africaMaxBounds = this.africaMaxBounds.bind(this);
     this.state = {
       markerPosition: [0, 0],
       scrolled: 0,
-      data: {
-        coordinates: [[]]
-      }
+      geoJSONData: []
     };
   }
 
   componentWillMount() {
     const self = this;
-    fetch('/flat/map.json')
+    const regionIds = [2, 3, 5];
+    //const regionIds = [2, 3, 5, 6];
+    regionIds.map(id => fetch(`/region/${id}/geojson_map.json`)
       .then(r => r.json())
-      .then(d => self.setState({ data: d }));
+      .then(d => {
+        let obj = {};
+        if (d.coordinates.length > 1) {
+          obj = {
+            id,
+            type: d.type,
+            coordinates: d.coordinates.map(self.flatten)
+          };
+        } else {
+          obj = Object.assign(d, { id });
+        }
+        const dataObjs = self.state.geoJSONData;
+        dataObjs.push(obj);
+        return self.setState({ geoJSONData: dataObjs });
+      })
+    );
   }
 
   onZoomEnd(e) {
@@ -46,38 +62,78 @@ export default class MapContainer extends Component {
     return [lats[lats.length / 2], longs[longs.length / 2]];
   }
 
+  flatten(ary) {
+    ary.reduce((a, b) => a.concat(b));
+    return ary;
+  }
+
+
   handleClick(e) {
     window.location = e.target.options.href;
   }
 
+  africaMaxBounds() {
+    return [
+      [38.5730952, -20.8207639],
+      [-40.7265232, 65.6147961]
+    ];
+  }
+
+  regionMeta() {
+    return {
+      2: {
+        className: 'central-africa',
+        title: 'Central Africa',
+        color: '#60D085'
+      },
+      3: {
+        className: 'eastern-africa',
+        title: 'Eastern Africa',
+        color: '#6FD4F2'
+      },
+      5: {
+        className: 'west-africa',
+        title: 'West Africa',
+        color: '#9DDC52'
+      },
+      6: {
+        className: 'southern-africa',
+        title: 'Southern Africa',
+        color: '#75E7D1'
+      }
+    };
+  }
+
   render() {
     /* eslint no-unused-vars: [0] */
-    const center = this.getCenter(this.state.data.coordinates);
+    // const center = this.getCenter(this.state.data.coordinates);
     const icon = divIcon({
-      className: 'my-div-icon',
+      className: 'leaflet-marker-icon',
       html: '<h1 class="leaflet-marker-icon__label">Hi, Jenna! Custom text</h1>'
     });
+    let geoJSONObjs = [];
+    if (this.state.geoJSONData) {
+      const self = this;
+      geoJSONObjs = this.state.geoJSONData.map((d) => <GeoJson
+        key={d.id}
+        href="http://google.com"
+        data={d}
+        className={self.regionMeta()[d.id].className}
+      />);
+    }
     return (
       <Map
         center={this.state.markerPosition}
-        zoom={5}
+        zoom={4}
         minZoom={4}
-        maxBounds={[[38.5730952, -20.8207639], [-40.7265232, 65.6147961]]}
+        maxBounds={this.africaMaxBounds()}
         maxZoom={12}
         onZoomEnd={this.onZoomEnd}
       >
         <TileLayer
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         />
-        <GeoJson
-          href="http://google.com"
-          data={this.state.data}
-          onClick={this.handleClick}
-        />
-        <Marker
-          position={center}
-          icon={icon}
-        />
+        {geoJSONObjs}
       </Map>
     );
   }
