2cd3a454f3476914a7082e547e676e0185c59518
Nick Cox <nick@nickcox.me>
2016-06-23 15:25:31 -0700

    remove unsed props

--- source/geometa.js
+++ source/geometa.js
@@ -3,13 +3,11 @@ export default {
     2: {
       className: 'central-africa',
       title: 'Central Africa',
-      color: '#60D085',
       href: '/2013/central-africa'
     },
     3: {
       className: 'eastern-africa',
       title: 'Eastern Africa',
-      color: '#6FD4F2',
       href: '/2013/eastern-africa'
     },
     4: {
@@ -21,13 +19,11 @@ export default {
     5: {
       className: 'west-africa',
       title: 'West Africa',
-      color: '#9DDC52',
       href: '/2013/west-africa'
     },
     6: {
       className: 'southern-africa',
       title: 'Southern Africa',
-      color: '#75E7D1',
       href: '/2013/southern-africa'
     }
   }
