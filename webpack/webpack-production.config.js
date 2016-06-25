775bafa9992fd3740745d09672ebac5f1af1c9ae
hudakdidit <hudak.jonathan@gmail.com>
2016-06-25 13:34:43 -0700

    Compilation of Stylus via Webpack instead of Gulp
    - Hot Loading works for CSS also
    - Compiles with autoprefixer
    - Configured production webpack config with Extract Text Plugin for
    compiled css build.
    - Setting `process.env.NODE_ENV` to ‘production’ disables React’s
    warnings about compressed development version.

--- webpack/webpack-production.config.js
+++ webpack/webpack-production.config.js
@@ -1,6 +1,7 @@
-var path = require('path');
-var webpack = require('webpack');
-var Common = require('./webpack-common.config');
+const path = require('path');
+const webpack = require('webpack');
+const Common = require('./webpack-common.config');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -14,12 +15,36 @@ module.exports = {
   plugins: [
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
-      __DEV: false
+      __DEV: false,
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new ExtractTextPlugin('css/main.css', { allChunks: true }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: false
+      }
     })
   ],
   module: {
     preLoaders: Common.preLoaders,
-    loaders: Common.loaders
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, '../source')
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|woff|otf|ttf|eot)$/, loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('stylus', 'css-loader!stylus-loader')
+      }
+    ]
   },
-  resolve: Common.resolve
+  resolve: Common.resolve,
+  stylus: Common.stylus
 };
