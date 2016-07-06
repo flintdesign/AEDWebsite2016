d7ad79415193181b905c1c16485b33eb7c118d39
Nate Bailey <nathanaelbailey@gmail.com>
2016-07-05 20:32:12 -0700

    Improved full-width sidebar styling for subGeographies

--- source/components/add_sidebar.js
+++ source/components/add_sidebar.js
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 import isEmpty from 'lodash.isempty';
 import ParentADD from './parent_add';
 import CountsBySubGeography from './counts_by_subgeography';
-import TotalCounts from './total_counts';
 import CountsBySurveyCategory from './counts_by_survey_category';
 import { pluralize, getNextGeography } from '../utils/convenience_funcs';
 
@@ -28,24 +27,11 @@ export default function ADDSidebar(props) {
 
       {!isEmpty(geographies) && currentTitle === 'totals' &&
         <div>
-          <TotalCounts
-            currentGeography={currentGeography}
-            year={year}
-            total={data('summary').ESTIMATE}
-            confidence={data('summary').CONFIDENCE}
-            guess_min={data('summary').GUESS_MIN}
-            guess_max={data('summary').GUESS_MAX}
-            range_covered={data(pluralize(subGeography)).PERCENT_OF_RANGE_COVERED}
-            range_assessed={data(pluralize(subGeography)).PERCENT_OF_RANGE_ASSESSED}
-            range_area={data(pluralize(subGeography)).RANGE_AREA}
-            iqi={data(pluralize(subGeography)).IQI}
-            pfs={data(pluralize(subGeography)).PFS}
-          />
-
           <CountsBySubGeography
             geographies={geographies[pluralize(subGeography)]}
             subGeography={subGeography}
             sidebarState={sidebarState}
+            totals={{ ...data('summary'), ...data(pluralize(subGeography)) }}
           />
         </div>
       }
--- source/components/counts_by_subgeography.js
+++ source/components/counts_by_subgeography.js
@@ -4,7 +4,7 @@ import { capitalize } from '../utils/convenience_funcs.js';
 import { SIDEBAR_FULL } from '../constants';
 
 export default function CountsBySubGeography(props) {
-  const { geographies, subGeography, sidebarState } = props;
+  const { geographies, subGeography, sidebarState, totals } = props;
 
   let markup = null;
   if (geographies) {
@@ -38,34 +38,55 @@ export default function CountsBySubGeography(props) {
           <table className="subgeography-totals">
             <thead>
               <tr>
-                <th>Country</th>
+                <th></th>
+                <th colSpan="2" style={{ textAlign: 'center' }}>Estimates from surveys</th>
+                <th colSpan="2" style={{ textAlign: 'center' }}>Guesses</th>
+                <th rowSpan="2">Range Area</th>
+                <th rowSpan="2">% of<br />Regional<br />Range</th>
+                <th rowSpan="2" className="subgeography-totals__subgeography-name">
+                  % of<br />Range<br />Assessed
+                </th>
+                <th rowSpan="2">IQI</th>
+                <th rowSpan="2">PFS</th>
+              </tr>
+              <tr>
+                <th className="subgeography-totals__subgeography-name">Country</th>
                 <th>Estimate</th>
                 <th>+- 95% CL</th>
-                <th>FROM</th>
-                <th>TO</th>
-                <th>Range Area</th>
-                <th>% of Regional Range</th>
-                <th>% of Range Assessed</th>
-                <th>IQI</th>
-                <th>PFS</th>
+                <th>From</th>
+                <th>To</th>
               </tr>
             </thead>
-            <tbody>{geographies.map((g, i) => (
-              <tr key={i}>
-                <td className="subgeography-totals__subgeography-name">
-                  {g[subGeography]}
-                </td>
-                <td>{formatNumber(g.ESTIMATE)}</td>
-                <td>{formatNumber(g.CONFIDENCE)}</td>
-                <td>{formatNumber(g.GUESS_MIN)}</td>
-                <td>{formatNumber(g.GUESS_MAX)}</td>
-                <td>{formatNumber(g.RANGE_AREA)}</td>
-                <td>{formatNumber(g.PERCENT_OF_RANGE_COVERED)}</td>
-                <td>{formatNumber(g.PERCENT_OF_RANGE_ASSESSED)}</td>
-                <td>{formatNumber(g.IQI)}</td>
-                <td>{formatNumber(g.PFS)}</td>
+            <tbody>
+              {geographies.map((g, i) => (
+                <tr key={i}>
+                  <td className="subgeography-totals__subgeography-name">
+                    {g[subGeography]}
+                  </td>
+                  <td>{formatNumber(g.ESTIMATE)}</td>
+                  <td>{formatNumber(g.CONFIDENCE)}</td>
+                  <td>{formatNumber(g.GUESS_MIN)}</td>
+                  <td>{formatNumber(g.GUESS_MAX)}</td>
+                  <td>{formatNumber(g.RANGE_AREA)}</td>
+                  <td>{formatNumber(g.PERCENT_OF_RANGE_COVERED)}</td>
+                  <td>{formatNumber(g.PERCENT_OF_RANGE_ASSESSED)}</td>
+                  <td>{formatNumber(g.IQI)}</td>
+                  <td>{formatNumber(g.PFS)}</td>
+                </tr>
+              ))}
+              <tr className="subgeography-totals__totals" key="totals">
+                <td className="subgeography-totals__subgeography-name">Totals</td>
+                <td>{formatNumber(totals.ESTIMATE)}</td>
+                <td>{formatNumber(totals.CONFIDENCE)}</td>
+                <td>{formatNumber(totals.GUESS_MIN)}</td>
+                <td>{formatNumber(totals.GUESS_MAX)}</td>
+                <td>{formatNumber(totals.RANGE_AREA)}</td>
+                <td>{formatNumber(totals.PERCENT_OF_RANGE_COVERED)}</td>
+                <td>{formatNumber(totals.PERCENT_OF_RANGE_ASSESSED)}</td>
+                <td>{formatNumber(totals.IQI)}</td>
+                <td>{formatNumber(totals.PFS)}</td>
               </tr>
-            ))}</tbody>
+            </tbody>
           </table>
         </div>
       );
@@ -79,4 +100,5 @@ CountsBySubGeography.propTypes = {
   geographies: PropTypes.array,
   subGeography: PropTypes.string.isRequired,
   sidebarState: PropTypes.number.isRequired,
+  totals: PropTypes.object.isRequired,
 };
