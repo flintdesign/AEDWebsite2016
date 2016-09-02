87847fa4280cd08921950b325960a86e1894d9b6
simmonsjenna <simmons.jennae@gmail.com>
2016-09-02 10:03:15 -0700

    add and style intro video container

--- source/components/pages/intro.js
+++ source/components/pages/intro.js
@@ -5,6 +5,13 @@ export default function Intro(props) {
   const introClass = `intro ${showIntro ? 'shown' : 'dismissed'}`;
   return (
     <div className={introClass} onClick={handleIntroClick}>
+      <div className={'intro-bg'}>
+          <video loop muted autoPlay poster="images/intro-video.mp4" className={'intro-bg__video'}>
+              <source src="images/intro-video.webm" type="video/webm" />
+              <source src="images/intro-video.mp4" type="video/mp4" />
+              <source src="images/intro-video.ogv" type="video/ogg" />
+          </video>
+      </div>
       <div className={'intro__container'}>
         <div className={'row'}>
           <div className={'col-two-thirds'}>
