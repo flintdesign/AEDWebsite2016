4c31b979fab41513c5ca67d0183b5b6732b35232
simmonsjenna <simmons.jennae@gmail.com>
2016-09-13 12:58:47 -0700

    update intro copy

--- source/components/pages/intro.js
+++ source/components/pages/intro.js
@@ -6,11 +6,6 @@ export default function Intro(props) {
   return (
     <div className={introClass} onClick={handleIntroClick}>
       <div className={'intro-bg'}>
-          <video loop muted autoPlay poster="images/intro-video.mp4" className={'intro-bg__video'}>
-              <source src="/images/intro-video.webm" type="video/webm" />
-              <source src="/images/intro-video.mp4" type="video/mp4" />
-              <source src="/images/intro-video.ogv" type="video/ogg" />
-          </video>
       </div>
       <div className={'intro__container'}>
         <div className={'row'}>
@@ -28,20 +23,29 @@ export default function Intro(props) {
               <div className={'intro__inner-content'}>
                 <div className="intro__description">
                   <p>
-                    Welcome to the African Elephant Database (AED), the most comprehensive database
-                    on the status of any species of mammal in the wild. The database is maintained
-                    by the African Elephant Specialist Group (AfESG), and holds data on elephant
-                    abundance and distribution across 37 countries where African elephants occur.
-                    It provides information on changes in elephant numbers and shows where these
-                    hanges have taken place. The database has been developed over 25 years and
+                    Welcome to the African Elephant Database (AED), the most comprehensive
+                    database on the status of any species of mammal in the wild. The
+                    database is maintained by the African Elephant Specialist Group (AfESG),
+                    and holds data on elephant numbers and distribution across the 37
+                    countries where African elephants occur. It provides information on
+                    changes in elephant numbers and shows where these changes have taken
+                    place. The database has been developed over the past 25 years and
                     went online in 2012.
                   </p>
                   <p>
-                    The AED relies on the contributions of countless survey biologists and experts
-                    across the continent.
-                    To learn more about the way the data is processed, and presented in tables and
-                    maps and the methods used for establishing summaries at the national, regional
-                    and continental level, visit our <a href="/about">About</a> section.
+                    Conservation and management of elephant populations and their habitats
+                    is critical and a goal towards which many individuals, organizations
+                    and countries are working. Even if opinions differ as to how best
+                    this can be achieved, it is widely agreed that decisions should be
+                    informed by the most up-to-date and reliable information available on
+                    the status of Africa’s elephants. The AfESG maintains the AED to meet
+                    this need and provides comprehensive information for individual
+                    countries, regions, and for Africa as a whole. The AED relies on the
+                    contributions of countless survey biologists, experts and observers
+                    across the continent. To learn more about the way the data is processed,
+                    compiled and presented in tables and maps and the methods used for
+                    establishing summaries at the national, regional and
+                    continental level, visit our <a href="/about">About</a> section.
                   </p>
                   <small>
                     All materials on this site are Copyright (C) 1995-2016 IUCN - The International
