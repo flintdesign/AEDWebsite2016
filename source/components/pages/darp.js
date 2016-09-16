7e1de38de45886d3b50db0813ad6481bb4974d96
Kevin Bleich <dmak78@gmail.com>
2016-09-16 15:09:30 -0700

    added references content and styles

--- source/components/pages/darp.js
+++ source/components/pages/darp.js
@@ -4,10 +4,13 @@ import DarpMarkup from 'html!./../../data/about/darp.html';
 export default function DARP() {
   return (
     <div className="darp">
-      <div className="darp__header">
-        <h1>Data Access and Release Policy</h1>
+      <div className="darp__inner">
+        <div className="darp__header">
+          <a className="darp__header__logo" href="/" />
+          <h1>Data Access and Release Policy</h1>
+        </div>
+        <div className="darp__content" dangerouslySetInnerHTML={ { __html: DarpMarkup } } />
       </div>
-      <div className="darp__content" dangerouslySetInnerHTML={ { __html: DarpMarkup } } />
     </div>
   );
 }
